001706aedb8e931e4555abc9261aa2800a8128ba
satriyobagus <satriyobgs1@gmail.com>
2024-04-17 08:26:47 +0800

    Memanggil method dari banyak Class di package lain

---
+++ Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen ======================");
+  System.out.println("NIDN : 1103058602");
+  System.out.Println("Nama : Mirza Yogy Kurniawan");
+  System.out.Println("====================================");
+ }
+}
